6568b715ae3af2413a5312dc37bf2cc46dade58c
Christoph Hellwig <hch@lst.de>
2025-07-10 15:33:34 +0200

    iomap: export iomap_writeback_folio
    Allow fuse to use iomap_writeback_folio for folio laundering.  Note
    that the caller needs to manually submit the pending writeback context.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Joanne Koong <joannelkoong@gmail.com>
    Reviewed-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/iomap/buffered-io.c
+++ fs/iomap/buffered-io.c
@@ -1637,8 +1637,7 @@ static bool iomap_writeback_handle_eof(struct folio *folio, struct inode *inode,
 	return true;
 }
 
-static int iomap_writeback_folio(struct iomap_writepage_ctx *wpc,
-		struct folio *folio)
+int iomap_writeback_folio(struct iomap_writepage_ctx *wpc, struct folio *folio)
 {
 	struct iomap_folio_state *ifs = folio->private;
 	struct inode *inode = wpc->inode;
@@ -1720,6 +1719,7 @@ static int iomap_writeback_folio(struct iomap_writepage_ctx *wpc,
 	mapping_set_error(inode->i_mapping, error);
 	return error;
 }
+EXPORT_SYMBOL_GPL(iomap_writeback_folio);
 
 int
 iomap_writepages(struct iomap_writepage_ctx *wpc)
