e8028935868c26c3076ba4159640db901ac14294
Yu Kuai <yukuai3@huawei.com>
2025-10-10 17:14:32 +0800

    mm/page_io: don't nest queue_lock under rcu in bio_associate_blkg_from_page()
    Prepare to convert protecting blkcg with blkcg_mutex instead of
    queue_lock.
    
    Signed-off-by: Yu Kuai <yukuai3@huawei.com>

--- mm/page_io.c
+++ mm/page_io.c
@@ -313,8 +313,13 @@ static void bio_associate_blkg_from_page(struct bio *bio, struct folio *folio)
 
 	rcu_read_lock();
 	css = cgroup_e_css(memcg->css.cgroup, &io_cgrp_subsys);
-	bio_associate_blkg_from_css(bio, css);
+	if (!css || !css_tryget_online(css))
+		css = NULL;
 	rcu_read_unlock();
+
+	bio_associate_blkg_from_css(bio, css);
+	if (css)
+		css_put(css);
 }
 #else
 #define bio_associate_blkg_from_page(bio, folio)		do { } while (0)
