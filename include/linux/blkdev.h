ddbebb34066df6d406f614f5d98e74df18835f38
Bart Van Assche <bvanassche@acm.org>
2025-11-07 15:52:59 -0800

    blk-zoned: Add an argument to blk_zone_plug_bio()
    Software that submits zoned writes, e.g. a filesystem, may submit zoned
    writes from multiple CPU cores as long as the zoned writes are serialized
    per zone. Submitting bios from different CPUs may cause bio reordering if
    e.g. different bios reach the storage device through different queues.
    Prepare for preserving the order of pipelined zoned writes per zone by
    adding the 'rq_cpu` argument to blk_zone_plug_bio(). This argument tells
    blk_zone_plug_bio() from which CPU a cached request has been allocated.
    The cached request will only be used if it matches the CPU from which
    zoned writes are being submitted for the zone associated with the bio.
    
    Cc: Damien Le Moal <dlemoal@kernel.org>
    Cc: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -925,7 +925,7 @@ static inline bool bio_needs_zone_write_plugging(struct bio *bio)
 	}
 }
 
-bool blk_zone_plug_bio(struct bio *bio, unsigned int nr_segs);
+bool blk_zone_plug_bio(struct bio *bio, unsigned int nr_segs, int rq_cpu);
 
 /**
  * disk_zone_capacity - returns the zone capacity of zone containing @sector
@@ -973,7 +973,8 @@ static inline bool bio_needs_zone_write_plugging(struct bio *bio)
 	return false;
 }
 
-static inline bool blk_zone_plug_bio(struct bio *bio, unsigned int nr_segs)
+static inline bool blk_zone_plug_bio(struct bio *bio, unsigned int nr_segs,
+				     int rq_cpu)
 {
 	return false;
 }
