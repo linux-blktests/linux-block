15e68bd070b9ae595643de73726a44d3e0945de5
Chaohai Chen <wdhh6@aliyun.com>
2026-01-16 14:19:27 +0800

    blk-mq-sched: Remove redundant code in blk_mq_sched_mark_restart_hctx().
    The current purpose of the blk_mq_sched_mark_restart_hctx() function
    is to set the BLK_MQ_S_SCHED_RESTART flag in hctx->state. Just remove
    the redundant judgement.
    
    Signed-off-by: Chaohai Chen <wdhh6@aliyun.com>

--- block/blk-mq-sched.c
+++ block/blk-mq-sched.c
@@ -21,9 +21,6 @@
  */
 void blk_mq_sched_mark_restart_hctx(struct blk_mq_hw_ctx *hctx)
 {
-	if (test_bit(BLK_MQ_S_SCHED_RESTART, &hctx->state))
-		return;
-
 	set_bit(BLK_MQ_S_SCHED_RESTART, &hctx->state);
 }
 EXPORT_SYMBOL_GPL(blk_mq_sched_mark_restart_hctx);
