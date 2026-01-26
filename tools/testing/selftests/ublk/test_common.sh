83049530975ee9c361a8999b690f381e7f984a70
Alexander Atanasov <alex@zazolabs.com>
2026-01-26 14:31:57 +0000

    selftests: ublk: mark each test start and end time in dmesg
    Log test start and end time in dmesg, so generated log messages
    during the test run can be linked to specific test from the test
    suite.
    
    Signed-off-by: Alexander Atanasov <alex@zazolabs.com>

--- tools/testing/selftests/ublk/test_common.sh
+++ tools/testing/selftests/ublk/test_common.sh
@@ -121,6 +121,7 @@ _prep_test() {
 	modprobe ublk_drv > /dev/null 2>&1
 	UBLK_TMP=$(mktemp ublk_test_XXXXX)
 	[ "$UBLK_TEST_QUIET" -eq 0 ] && echo "ublk $type: $*"
+	echo "ublk selftest: $TID starting at $(date --iso-8601=sec)" | tee /dev/kmsg
 }
 
 _remove_test_files()
@@ -165,6 +166,7 @@ _cleanup_test() {
 	"${UBLK_PROG}" del -a
 
 	_remove_files
+	echo "ublk selftest: $TID done at $(date --iso-8601=sec)" | tee /dev/kmsg
 }
 
 _have_feature()
