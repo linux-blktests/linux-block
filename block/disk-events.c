3f97f94b1495f7eb5f462b8cd29dcc7868255f4a
Jackie Liu <liuyun01@kylinos.cn>
2026-04-01 15:07:14 +0800

    block: use kstrtol() instead of sscanf() in disk_events_poll_msecs_store()
    kstrtol() is the preferred way to parse a single integer from sysfs
    input. It also makes the !count check unnecessary since kstrtol()
    returns -EINVAL on empty or malformed strings.
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>

--- block/disk-events.c
+++ block/disk-events.c
@@ -373,7 +373,7 @@ static ssize_t disk_events_poll_msecs_store(struct device *dev,
 	struct gendisk *disk = dev_to_disk(dev);
 	long intv;
 
-	if (!count || !sscanf(buf, "%ld", &intv))
+	if (kstrtol(buf, 10, &intv))
 		return -EINVAL;
 
 	if (intv < 0 && intv != -1)
