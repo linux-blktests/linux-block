adc095b2cadfd9d4510bd90ad02ab819a9ee079e
Christoph Hellwig <hch@lst.de>
2026-05-15 06:55:34 +0200

    block: unexport bio_{set,check}_pages_dirty
    Only used in built-in code.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- block/bio.c
+++ block/bio.c
@@ -1653,7 +1653,6 @@ void bio_set_pages_dirty(struct bio *bio)
 		folio_unlock(fi.folio);
 	}
 }
-EXPORT_SYMBOL_GPL(bio_set_pages_dirty);
 
 /*
  * bio_check_pages_dirty() will check that all the BIO's pages are still dirty.
@@ -1712,7 +1711,6 @@ defer:
 	spin_unlock_irqrestore(&bio_dirty_lock, flags);
 	schedule_work(&bio_dirty_work);
 }
-EXPORT_SYMBOL_GPL(bio_check_pages_dirty);
 
 static inline bool bio_remaining_done(struct bio *bio)
 {
