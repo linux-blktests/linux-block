e8e62705ed271c0c2a0c87c2f9422f54c4524736
Keith Busch <kbusch@kernel.org>
2026-06-22 10:42:38 -0700

    block: fix dio leak on metadata mapping error
    A failed integrity mapping holds a dio reference, so we need to go
    through the full bio ending in case there were previously submitted
    bio's in the sequence.
    
    Fixes: 2729a60bbfb92 ("block: don't silently ignore metadata for sync read/write")
    Signed-off-by: Keith Busch <kbusch@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- block/fops.c
+++ block/fops.c
@@ -238,8 +238,10 @@ static ssize_t __blkdev_direct_IO(struct kiocb *iocb, struct iov_iter *iter,
 		}
 		if (iocb->ki_flags & IOCB_HAS_METADATA) {
 			ret = bio_integrity_map_iter(bio, iocb->private);
-			if (unlikely(ret))
-				goto fail;
+			if (unlikely(ret)) {
+				bio_endio_errno(bio, ret);
+				break;
+			}
 		}
 
 		if (is_read) {
