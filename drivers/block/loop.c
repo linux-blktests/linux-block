38926bc0d77de4ec1fa1756e61892351ac8204d1
Bart Van Assche <bvanassche@acm.org>
2026-08-20 12:57:10 -0700

    loop: Reorder checks in loop_validate_file()
    Checking inode attributes after a loop that modifies the file pointer
    the inode has been derived from confuses some static analyzers. Hence
    swap the loop and the inode attribute check.
    
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>

--- drivers/block/loop.c
+++ drivers/block/loop.c
@@ -508,6 +508,9 @@ static int loop_validate_file(struct file *file, struct block_device *bdev)
 	struct inode	*inode = file->f_mapping->host;
 	struct file	*f = file;
 
+	if (!S_ISREG(inode->i_mode) && !S_ISBLK(inode->i_mode))
+		return -EINVAL;
+
 	/* Avoid recursion */
 	while (is_loop_device(f)) {
 		struct loop_device *l;
@@ -524,8 +527,6 @@ static int loop_validate_file(struct file *file, struct block_device *bdev)
 		rmb();
 		f = l->lo_backing_file;
 	}
-	if (!S_ISREG(inode->i_mode) && !S_ISBLK(inode->i_mode))
-		return -EINVAL;
 	return 0;
 }
 
